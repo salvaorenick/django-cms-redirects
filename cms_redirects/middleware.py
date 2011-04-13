16bfefd6d9521f76bd82ca096b51b4de4452a118
Mark Ransom <megamark16@gmail.com>
2011-04-13 13:54:45 -0500

    Switched the middleware over to using process_exception

--- cms_redirects/middleware.py
+++ cms_redirects/middleware.py
@@ -3,6 +3,7 @@ from django import http
 from django.conf import settings
 
 class RedirectFallbackMiddleware(object):
+    """
     def process_response(self, request, response):
         if response.status_code != 404:
             return response # No need to check for a redirect for non-404 responses.
@@ -27,3 +28,25 @@ class RedirectFallbackMiddleware(object):
 
         # No redirect was found. Return the response.
         return response
+    """
+    def process_exception(self, request, exception):
+        if isinstance(exception, Http404):
+            path = request.get_full_path()
+        try:
+            r = CMSRedirect.objects.get(site__id__exact=settings.SITE_ID, old_path=path)
+        except CMSRedirect.DoesNotExist:
+            r = None
+        if r is None and settings.APPEND_SLASH:
+            # Try removing the trailing slash.
+            try:
+                r = CMSRedirect.objects.get(site__id__exact=settings.SITE_ID,
+                    old_path=path[:path.rfind('/')]+path[path.rfind('/')+1:])
+            except CMSRedirect.DoesNotExist:
+                pass
+        if r is not None:
+            if r.page:
+                return http.HttpResponsePermanentRedirect(r.page.get_absolute_url())
+            if r.new_path == '':
+                return http.HttpResponseGone()
+            return http.HttpResponsePermanentRedirect(r.new_path)
+
