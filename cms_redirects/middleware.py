39400cef2f25a289fc9e16b2f53e57effc456074
miv <mike.verdone@ableton.com>
2011-11-11 15:53:32 +0100

    Try removing query string when looking for redirects.
    If a redirect URL has a query at the end, eg. /foo?bar=quux, it can now be
    routed properly as /foo. We lookup the redirect with the query first, so
    one can create redirects with specific query requirements. However, if
    we cant find one, we search again without the query string, which is usually
    better than just giving up entirely.
    
    In most cases, the query is just analytics parameters like orig_ql or utm_*.

--- cms_redirects/middleware.py
+++ cms_redirects/middleware.py
@@ -3,21 +3,35 @@ from cms_redirects.models import CMSRedirect
 from django import http
 from django.conf import settings
 
+def get_redirect(old_path):
+    try:
+        r = CMSRedirect.objects.get(site__id__exact=settings.SITE_ID,
+                                    old_path=old_path)
+    except CMSRedirect.DoesNotExist:
+        r = None
+    return r
+
+def remove_slash(path):
+    return path[:path.rfind('/')]+path[path.rfind('/')+1:]
+
+def remove_query(path):
+    return path.split('?', 1)[0]
+
 class RedirectFallbackMiddleware(object):
     def process_exception(self, request, exception):
         if isinstance(exception, http.Http404):
             path = request.environ.get('PATH_INFO')
-            try:
-                r = CMSRedirect.objects.get(site__id__exact=settings.SITE_ID, old_path=path)
-            except CMSRedirect.DoesNotExist:
-                r = None
+            r = get_redirect(path)
+
+            # If we couldn't find the path, try removing the slash
+            # and/or removing the query string.
             if r is None and settings.APPEND_SLASH:
-                # Try removing the trailing slash.
-                try:
-                    r = CMSRedirect.objects.get(site__id__exact=settings.SITE_ID,
-                        old_path=path[:path.rfind('/')]+path[path.rfind('/')+1:])
-                except CMSRedirect.DoesNotExist:
-                    pass
+                r = get_redirect(remove_slash(path))
+            if r is None and path.count('?'):
+                r = get_redirect(remove_query(path))
+            if r is None and path.count('?') and settings.APPEND_SLASH:
+                r = get_redirect(remove_slash(remove_query(path)))
+
             if r is not None:
                 if r.page:
                     if r.response_code == '302':
@@ -30,5 +44,3 @@ class RedirectFallbackMiddleware(object):
                     return http.HttpResponseRedirect(r.new_path)
                 else:
                     return http.HttpResponsePermanentRedirect(r.new_path)
-        
-
