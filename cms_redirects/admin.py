bdc4052413e6b5440e15272195cfca52e5bff71f
Jordan Bouvier <jbouvier@barackobama.com>
2014-05-09 14:51:28 -0500

    PEP8 cleanup

--- cms_redirects/admin.py
+++ cms_redirects/admin.py
@@ -1,18 +1,24 @@
+"""Admin setup for redirects."""
 from django.contrib import admin
 from cms_redirects.models import CMSRedirect
 
+
 class CMSRedirectAdmin(admin.ModelAdmin):
-    list_display = ('old_path', 'new_path', 'page', 'page_site', 'site', 'actual_response_code',)
+    """Admin configuration for redirects"""
+    list_display = (
+        'old_path',
+        'new_path',
+        'page',
+        'page_site',
+        'site',
+        'actual_response_code'
+    )
     list_filter = ('site',)
     search_fields = ('old_path', 'new_path', 'page__title_set__title')
     radio_fields = {'site': admin.VERTICAL}
     fieldsets = [
-        ('Source', {
-            "fields": ('site','old_path',)
-        }),
-        ('Destination', {
-            "fields": ('new_path','page', 'response_code',)
-        }),
+        ('Source', {'fields': ('site', 'old_path')}),
+        ('Destination', {'fields': ('new_path', 'page', 'response_code')})
     ]
 
 admin.site.register(CMSRedirect, CMSRedirectAdmin)
--- cms_redirects/models.py
+++ cms_redirects/models.py
@@ -1,32 +1,58 @@
+"""Models for cms redirects."""
 from django.db import models
 from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
 from cms.models.fields import PageField
 
-from cms.models import Page
 
 RESPONSE_CODES = (
     ('301', '301'),
-    ('302', '302'),
+    ('302', '302')
 )
 
+
 class CMSRedirect(models.Model):
-    page = PageField(verbose_name=_("page"), blank=True, null=True, help_text=_("A link to a page has priority over a text link."))
+    """Model for information about a redirect"""
+    page = PageField(
+        verbose_name=_("page"),
+        blank=True,
+        null=True,
+        help_text=_("A link to a page has priority over a text link.")
+    )
     site = models.ForeignKey(Site)
-    old_path = models.CharField(_('redirect from'), max_length=200, db_index=True,
-        help_text=_("This should be an absolute path, excluding the domain name. Example: '/events/search/'."))
-    new_path = models.CharField(_('redirect to'), max_length=200, blank=True,
-        help_text=_("This can be either an absolute path (as above) or a full URL starting with 'http://'."))
-    response_code = models.CharField(_('response code'), max_length=3, choices=RESPONSE_CODES, default=RESPONSE_CODES[0][0],
-        help_text=_("This is the http response code returned if a destination is specified. If no destination is specified the response code will be 410."))
+    old_path = models.CharField(
+        verbose_name=_('redirect from'),
+        max_length=200,
+        db_index=True,
+        help_text=_("This should be an absolute path, excluding the"
+                    " domain name. Example: '/events/search/'.")
+    )
+    new_path = models.CharField(
+        verbose_name=_('redirect to'),
+        max_length=200,
+        blank=True,
+        help_text=_("This can be either an absolute path (as above) or a"
+                    " full URL starting with 'http://'.")
+    )
+    response_code = models.CharField(
+        verbose_name=_('response code'),
+        max_length=3,
+        choices=RESPONSE_CODES,
+        default=RESPONSE_CODES[0][0],
+        help_text=_("This is the http response code returned if a destination"
+                    " is specified. If no destination is specified"
+                    " the response code will be 410.")
+    )
     
     def page_site(self):
+        """If this redirects to a page, return the name of the site."""
         if self.page:
             return u'%s' % self.page.site
         return u''
     page_site.short_description = "Page Site"
     
     def actual_response_code(self):
+        """Returns the response code. Returns 410 if no redirect is defined."""
         if self.page or self.new_path:
             return self.response_code
         return u'410'
@@ -39,4 +65,5 @@ class CMSRedirect(models.Model):
         ordering = ('old_path',)
     
     def __unicode__(self):
+        """Unicode representation of this redirect."""
         return "%s ---> %s" % (self.old_path, self.new_path)
