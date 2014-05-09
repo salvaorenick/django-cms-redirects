252747ae81e57a79302c0dfe5732baf98ccf56f3
Jordan Bouvier <jbouvier@barackobama.com>
2014-05-09 12:48:38 -0500

    PEP8 cleanup on setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
 from setuptools import setup, find_packages
-import os
 
-version = __import__('cms_redirects').__version__
+version=__import__('cms_redirects').__version__
 
-install_requires = [
+install_requires=[
     'setuptools',
     'django',
     'django-cms',
 ]
 
 setup(
-    name = "django-cms-redirects",
-    version = version,
-    url = 'http://github.com/salvaorenick/django-cms-redirects',
-    license = 'BSD',
+    name="django-cms-redirects",
+    version=version,
+    url='http://github.com/salvaorenick/django-cms-redirects',
+    license='BSD',
     platforms=['OS Independent'],
-    description = "",
-    author = "Andrew Schoen",
-    author_email = 'andrew.schoen@gmail.com',
+    description="",
+    author="Andrew Schoen",
+    author_email='andrew.schoen@gmail.com',
     packages=find_packages(),
-    install_requires = install_requires,
+    install_requires=install_requires,
     include_package_data=True,
     zip_safe=False,
-    classifiers = [
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
         'Intended Audience :: Developers',
